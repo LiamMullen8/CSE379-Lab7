9fa4b91763ca4e1bffc38c7dd5b86454e954e62a
Marcos_ <DeveloperMarcos_@outlook.com>
2022-04-18 16:11:05 -0400

    Update Testing.s
    created a move_left simple cases, for primary testing and run analysis. Ideally this should work out the kinks in the game enough that we could test via the uart

--- Testing.s
+++ Testing.s
@@ -232,3 +232,235 @@
 
 ;;;---------------------End of Render Game Board Testing-------------------------;;;
 ;;;------------------------------------------------------------------------------;;;
+
+
+
+
+
+;;;------------------------------------------------------------------------------;;;
+;;;---------------------------Move & Merge Testing-------------------------------;;;
+;;;------------------------------------------------------------------------------;;;
+
+
+;;;------------------------------------------------------------------------------;;;
+;;		Simple move_left test, showing the merging of block2 in columns 1&2 -> column 1
+;;	[2 2 _ _]		[4 _ _ _]
+;;	[2 2 _ _]	to 	[4 _ _ _]
+;;	[2 2 _ _]		[4 _ _ _]
+;;	[2 2 _ _]		[4 _ _ _]
+
+	;; clear terminal display
+	LDR R0, ptr_to_clear_screen
+	BL output_string
+
+	;; Print Time score Prompt
+	LDR R0, ptr_to_timescore_prompt
+	BL output_string
+
+	;; display starting screen
+	LDR R0, ptr_to_game_board
+	BL output_string
+	
+	;-------Move Left------;
+	; Merge the block2 in Column 1 with the block2 in Column 2
+	
+	;---ROW 1---;
+	LDR R0, ptr_to_SQ0 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ1 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---ROW 2---;
+	LDR R0, ptr_to_SQ4 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ5 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---ROW 3---;
+	LDR R0, ptr_to_SQ8 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ9 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+
+	;---ROW 4---;
+	LDR R0, ptr_to_SQ12 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ13 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+;;;------------------------------------------------------------------------------;;;
+;;		Repeated call move_left calling 
+;;	[2 _ _ 2]	[2 _ 2 _]	[2 2 _ _]	[4 _ _ _]
+;;	[2 _ _ 2]	[2 _ 2 _]	[2 2 _ _]	[4 _ _ _]
+;;	[2 _ _ 2]	[2 _ 2 _]	[2 2 _ _]	[4 _ _ _]
+;;	[2 _ _ 2]	[2 _ 2 _]	[2 2 _ _]	[4 _ _ _]
+
+	;; clear terminal display
+	LDR R0, ptr_to_clear_screen
+	BL output_string
+
+	;; Print Time score Prompt
+	LDR R0, ptr_to_timescore_prompt
+	BL output_string
+
+	;; display starting screen
+	LDR R0, ptr_to_game_board
+	BL output_string
+
+	;---ROW 1---;
+	LDR R0, ptr_to_SQ0 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ3 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---ROW 2---;
+	LDR R0, ptr_to_SQ4 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ7 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---ROW 3---;
+	LDR R0, ptr_to_SQ8 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ11 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+
+	;---ROW 4---;
+	LDR R0, ptr_to_SQ12 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ15 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---Call & Render---;
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+;;;------------------------------------------------------------------------------;;;
+;;		Capped Merge Example 1 (held in the @)
+;;		[4 2 _ 2]	[4 2 2 _]	[4 4 _ _]	[4 4 _ _](Extra third call for the test)
+;;		[4 2 _ 2]	[4 2 2 _]	[4 4 _ _]	[4 4 _ _]
+;;		[4 2 _ 2]	[4 2 2 _]	[4 4 _ _]	[4 4 _ _]
+;;		[4 2 _ 2]	[4 2 2 _]	[4 4 _ _]	[4 4 _ _]
+
+	;; clear terminal display
+	LDR R0, ptr_to_clear_screen
+	BL output_string
+
+	;; Print Time score Prompt
+	LDR R0, ptr_to_timescore_prompt
+	BL output_string
+
+	;; display starting screen
+	LDR R0, ptr_to_game_board
+	BL output_string
+
+	;---ROW 1---;
+	LDR R0, ptr_to_SQ0 ;First block4
+	MOV R1, #0x4
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ1 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ3 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---ROW 2---;
+	LDR R0, ptr_to_SQ4 ;First block4
+	MOV R1, #0x4
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ5 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ7 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+
+	;---ROW 3---;
+	LDR R0, ptr_to_SQ8 ;First block4
+	MOV R1, #0x4
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ9 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ11 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+
+	;---ROW 4---;
+	LDR R0, ptr_to_SQ12 ;First block4
+	MOV R1, #0x4
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ13 ;First block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	LDR R0, ptr_to_SQ15 ;Second block2
+	MOV R1, #0x2
+	STR R1, [R0]
+	
+	;---Call & Render---;
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+	BL move_left
+	
+	BL render_game_board
+	
+;;;------------------------End of Move & Merge Testing---------------------------;;;
+;;;------------------------------------------------------------------------------;;;
